79dc3c5ab9a2622d0e809eef18777e0cbf86a690
chandu9605 <68846865+chandu9605@users.noreply.github.com>
2024-04-30 18:03:59 -0400

    identity

--- Bulky.DataAccess/Data/ApllicationDbContext.cs
+++ Bulky.DataAccess/Data/ApllicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Bulky.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bulky.DataAccess.Data
 {
-    public class ApllicationDbContext : DbContext
+    public class ApllicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApllicationDbContext(DbContextOptions<ApllicationDbContext> options) : base(options)
         {
@@ -12,7 +14,8 @@ namespace Bulky.DataAccess.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
+        { 
+            base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Category>().HasData(
                 new Category {Id=1,Name="Action",DisplayOrder=1 },
                 new Category { Id=2,Name="SciFi", DisplayOrder=2},
